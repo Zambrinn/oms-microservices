71aec16e23a907383a24adda18e0d10dfe6b4065
Zambrinn <zambrinfilho@gmail.com>
2026-09-13 17:28:48 -0300

    feat(orders): implement grpc handler

--- orders/main.go
+++ orders/main.go
@@ -1,10 +1,38 @@
 package main
 
-import "context"
+import (
+	"context"
+	"log"
+	"net"
+
+	common "github.com/Zambrinn/oms-common"
+	"google.golang.org/grpc"
+)
+
+var (
+	grpcAddr = common.EnvString("GRPC_ADDR", "localhost:2000")
+)
 
 func main() {
+	grpcServer := grpc.NewServer()
+
+	l, err := net.Listen("tcp", grpcAddr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	defer l.Close()
+
 	store := NewStore()
 	svc := newService(store)
+	NewGRPCHandler(grpcServer)
 
 	svc.CreateOrder(context.Background())
+
+	log.Println("GRPC Server Started at ", grpcAddr)
+	
+
+	if err := grpcServer.Serve(l); err != nil {
+		log.Fatal(err.Error())
+	}
 }
